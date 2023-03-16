bf873fc1a8579296f589fde63b2278bc887e6f50
SarahStudent <102655178+SarahStudent@users.noreply.github.com>
2023-03-16 16:13:49 +0100

    Update test.m

--- test.m
+++ test.m
@@ -1,7 +1,9 @@
+% diese Datei stellt ein Beispiel dar, wie findOptimalStrategies.m verwendet werden kann
+% WICHTIGER HINWEIS: es können entweder die optimalen Strategien ODER die zugehören Auszahlungen geplottet werden, beides ist rein technisch nicht möglich
 struct.g1 = {[5;0], [-1;-5], [4;-4]; [2;-2], [2;-7], [2;2]; [0;-6], [6;-2], [-2;4]}; % Matrix
 struct.g2 = {[-5;0],[-2;2],  [0;6];  [1;5],  [-2;7], [-6;2];[-4;4], [-2;-2],[2;-4]}; % -1* Matrix^T
 
-struct.poss = 1; % sollen optimale Menge an POSS Auszahlungen geplottet werden
+struct.poss = 1; % soll optimale Menge an POSS Auszahlungen geplottet werden
 
 struct.min = 0; % sollen minimale Strategien geplottet werden
 struct.max = 0; % sollen maximale Strategien geplottet werden
